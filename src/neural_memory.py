5fb0c9f27dce46da1dc76a5327d391ebb76d9c54
nyvyn <2886602+nyvyn@users.noreply.github.com>
2025-04-16 05:51:16 -0500

    Refactor comment formatting in persist method
    Improved readability of comments in the persist method by splitting a lengthy single-line comment into multiple lines. This change helps in better understanding and maintenance of the code.

--- src/neural_memory.py
+++ src/neural_memory.py
@@ -25,9 +25,11 @@ class NeuralMemory:
             pad_token_id=self.tokenizer.eos_token_id,
         )
         return self.tokenizer.decode(outputs[0][inputs['input_ids'].shape[1]:])
-    
+
     def persist(self, ctx):
-        # please make sure the context to inject into the memory is larger than 16 tokens, this is the hard minimum when training the model. The memory will be disturbed when less than 16 tokens are injected into the memory.
+        # please make sure the context to inject into the memory is larger than 16 tokens,
+        # this is the hard minimum when training the model.
+        # The memory will be disturbed when less than 16 tokens are injected into the memory.
         self.model.inject_memory(
             self.tokenizer(ctx, return_tensors='pt', add_special_tokens=False).input_ids.to(self.model.device),
             update_memory=True
