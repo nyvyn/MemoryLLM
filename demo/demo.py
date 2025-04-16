e8f19925685d9f25cb4280519b47c001b6c3be64
nyvyn <2886602+nyvyn@users.noreply.github.com>
2025-04-16 06:13:37 -0500

    Refactor print statements for improved output clarity
    Revised the print statements to separate prompts and responses explicitly. This improves readability and ensures better understanding of the input-output structure during debugging or testing.

--- demo/demo.py
+++ demo/demo.py
@@ -7,19 +7,28 @@ def main():
     print(f"### - Initial test, no context:")
     prompt = "Question: What foods does David like? Answer: David likes"
     response = nm.query(prompt, max_length=30)
-    print(prompt + response)
+    print("Prompt:")
+    print(prompt)
+    print("Output:")
+    print(response)
 
     print(f"### - In-context learning:")
     ctx = ("Last week, John had a wonderful picnic with David. During their conversation, David mentioned multiple "
            "times that he likes eating nuts. Though he didn't mention any other foods, John says he can infer that "
            "David also likes other salty foods.")
     response = nm.query(ctx + ' ' + prompt, max_length=90)
-    print(prompt + response)
+    print("Prompt:")
+    print(prompt)
+    print("Output:")
+    print(response)
 
     print(f"### - Memory injection")
     nm.persist(ctx)
     response = nm.query(prompt, max_length=30)
-    print(prompt + response)
+    print("Prompt:")
+    print(prompt)
+    print("Output:")
+    print(response)
 
 
 if __name__ == "__main__":
